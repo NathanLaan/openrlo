15c77b21226e8704989b50c06a87cd0f5624155c
Nathan Laan <nathanlaan@gmail.com>
2011-10-27 23:57:30 -0600

    Working on data structures.

---
+++ OpenRLO.Web/Data/LearningObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenRLO.Web.Data
+{
+
+
+  /*
+    - ID
+    - Name/Title
+    - Description
+    - DateTime
+    - Reference Links (list)
+    - Content
+    - Author
+   */
+  public class LearningObject
+  {
+  }
+}
