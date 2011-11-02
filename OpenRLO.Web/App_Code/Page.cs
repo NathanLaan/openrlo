ae14fc16cb1de55b59a3a72b04df15c938492de0
Nathan Laan <nathanlaan@gmail.com>
2011-11-02 15:39:51 -0600

    Adding data structure files.

---
+++ OpenRLO.Web/App_Code/Page.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenRLO.Web.App_Code
+{
+  public class Page
+  {
+  }
+}
---
+++ OpenRLO.Web/App_Code/Topic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenRLO.Web.App_Code
+{
+  public class Topic
+  {
+    public string Title { get; set; }
+  }
+}
