212a7f5719e3d30b88caf52164e60250895490b9
Nathan Laan <nathanlaan@gmail.com>
2011-11-14 09:17:16 -0700

    Updating WebSiteFlexHttpHandlerPlugin to support /Learn/Subject/Topic/Page URLs.

--- OpenRLO.Web/HttpHandler/WebSiteFlexHttpHandlerPlugin.cs
+++ OpenRLO.Web/HttpHandler/WebSiteFlexHttpHandlerPlugin.cs
@@ -10,83 +10,31 @@ namespace OpenRLO.Web.HttpHandler
     {
       FlexHttpHandlerResult result = null;
 
-      if (splitArray.Length > 3
-        && splitArray[1] == "admin"
-        && splitArray[2] == "article"
-        && splitArray[3] == "edit"
+      if (splitArray.Length > 4
+        && splitArray[1] == "learn"
+        && !string.IsNullOrEmpty(splitArray[2])
+        && !string.IsNullOrEmpty(splitArray[3])
         && !string.IsNullOrEmpty(splitArray[4]))
       {
-        result = new FlexHttpHandlerResult(@"/admin/AdminArticleEdit.aspx", "a=" + splitArray[4], "admin");
+        result = new FlexHttpHandlerResult(@"/View.aspx", "s=" + splitArray[2] + "&t=" + splitArray[3] + "&p=" + splitArray[4], "admin");
       }
 
-      if (splitArray.Length == 3
-        && splitArray[1] == "s"
+      if (splitArray.Length > 3
+        && splitArray[1] == "learn"
+        && !string.IsNullOrEmpty(splitArray[2])
+        && !string.IsNullOrEmpty(splitArray[3]))
+      {
+        result = new FlexHttpHandlerResult(@"/View.aspx", "s=" + splitArray[2] + "&t=" + splitArray[3], "admin");
+      }
+
+      if (splitArray.Length > 2
+        && splitArray[1] == "learn"
         && !string.IsNullOrEmpty(splitArray[2]))
       {
-        result = new FlexHttpHandlerResult(@"/ViewArticle.aspx", "s=" + splitArray[2], "article");
+        result = new FlexHttpHandlerResult(@"/View.aspx", "s=" + splitArray[2], "admin");
       }
 
-      //if (splitArray.Length == 1)
-      //{
-      //  //result = new FlexHttpHandlerResult(@"/ViewCategory.aspx", "c=" + splitArray[0], "category");
-      //  return new FlexHttpHandlerResult(@"/Default.aspx", "c=" + splitArray[0], "category");
-      //}
-      //else if (splitArray.Length == 2)
-      //{
-      //  //if (!string.IsNullOrEmpty(splitArray[1])) // splitArray[1] != null && splitArray[1] != string.Empty)
-      //  //{
-      //  //result = new FlexHttpHandlerResult(@"/ViewCategory.aspx", "c=" + splitArray[1], "category");
-      //  return new FlexHttpHandlerResult(@"/Default.aspx", "c=" + splitArray[1], "category");
-      //  //} 
-      //}
-      //else if (splitArray.Length == 3)
-      //{
-      //  if (!string.IsNullOrEmpty(splitArray[2]))
-      //  {
-      //    if (splitArray[1] == "page")
-      //    {
-      //      return new FlexHttpHandlerResult(@"/Default.aspx", "p=" + splitArray[2], "home");
-      //    }
-      //    else if (splitArray[1] == "s")
-      //    {
-      //      return new FlexHttpHandlerResult(@"/ViewArticle.aspx", "s=" + splitArray[2], "article");
-      //    }
-      //    //else
-      //    //{
-      //    //  return new FlexHttpHandlerResult(@"/ViewArticle.aspx", "c=" + splitArray[1] + "&a=" + splitArray[2], "article");
-      //    //}
-      //  }
-      //  else
-      //  {
-      //    //result = new FlexHttpHandlerResult(@"/ViewCategory.aspx", "c=" + splitArray[1], "category");
-      //    return new FlexHttpHandlerResult(@"/Default.aspx", "c=" + splitArray[1], "category");
-      //  }
-      //}
-      //else if (splitArray.Length > 3)
-      //{
-      //  // /11111/2222222/3333
-      //  // /admin/article/edit
-      //  if (splitArray[1] == "admin"
-      //    && splitArray[2] == "article"
-      //    && splitArray[3] == "edit"
-      //    && !string.IsNullOrEmpty(splitArray[4]))
-      //  {
-      //    return new FlexHttpHandlerResult(@"/admin/AdminArticleEdit.aspx", "a=" + splitArray[4], "admin");
-      //  }
-      //  else if (splitArray[2] == "page"
-      //    && !string.IsNullOrEmpty(splitArray[1])
-      //    && !string.IsNullOrEmpty(splitArray[3]))
-      //  {
-      //    //result = new FlexHttpHandlerResult(@"/ViewCategory.aspx", "c=" + splitArray[1] + "&p=" + splitArray[3], "category");
-      //    return new FlexHttpHandlerResult(@"/Default.aspx", "c=" + splitArray[1] + "&p=" + splitArray[3], "category");
-      //  }
-      //  else
-      //  {
-      //    //result = new FlexHttpHandlerResult(@"/ViewArticle.aspx", "c=" + splitArray[1] + "&a=" + splitArray[2], "category");
-      //    return new FlexHttpHandlerResult(@"/Default.aspx", "c=" + splitArray[1] + "&a=" + splitArray[2], "category");
-      //  }
-      //}
-      return result;// new FlexHttpHandlerResult(@"/Default.aspx", "", "home");
+      return result;
     }
   }
 }
